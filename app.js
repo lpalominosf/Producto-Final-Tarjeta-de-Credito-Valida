ccc7f36fcbc70098b41df860e145edecd089ea7f
Lore Palominos <lorena.palominos@gmail.com>
2018-01-24 22:37:47 -0300

    mini avance

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ function isValidCard(creditCardNumber) {
     */
     if (creditCardNumber === '' && creditCardNumber === ' ') {
       /*
-      *Los datos ingresados serán un string, que se convertira en un array para recorrer.
+      *Los datos ingresados serán un string, que se convertira en un array de números para recorrer.
       */
-      var arr = creditCardNumber.split('');
+      var array = creditCardNumber.split('');
     } else {
       alert('Ingrese un número válido');
     }
@@ -19,6 +19,16 @@ function isValidCard(creditCardNumber) {
     *While condiciona al usuario a ingresar un número válido de tarjeta
     */
   } while (creditCardNumber === '' || creditCardNumber === ' ');
-}
+  /*Método reverse, para dejar el array al revés*/
+  var arrayReverse = array.reverse();
+  var newArray = []; // Nuevo arreglo, este será de números, no un string.
+  /*
+  *For que recorre el array en reversa
+  */
+  for (var i = 0; i < arrayReverse.length; i++) {
+    newArray.push(parseInt(arrayReverse[i]));
+  }
+
+}//funcion
 
 isValidCard();
