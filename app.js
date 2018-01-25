dd131f06a262fe541f61649e348558a5f4943167
Lore Palominos <lorena.palominos@gmail.com>
2018-01-24 22:27:51 -0300

    funcion y prompt inicial ok

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-function isValidCard() {
+function isValidCard(creditCardNumber) {
   do {
     /*
     *Por medio de un prompt se le pide al usuario que ingrese el número de su tarjeta.
     */
-    creditCardNumber = prompt('Ingrese el número de su tarjeta');
-  }
+    var creditCardNumber = prompt('Ingrese el número de su tarjeta de crédito');
+    /*
+    *Condición para que usuario no ingrese un campo vacío.
+    */
+    if (creditCardNumber === '' && creditCardNumber === ' ') {
+      /*
+      *Los datos ingresados serán un string, que se convertira en un array para recorrer.
+      */
+      var arr = creditCardNumber.split('');
+    } else {
+      alert('Ingrese un número válido');
+    }
+    /*
+    *While condiciona al usuario a ingresar un número válido de tarjeta
+    */
+  } while (creditCardNumber === '' || creditCardNumber === ' ');
+}
+
+isValidCard();
